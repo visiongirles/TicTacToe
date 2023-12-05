ef32da609bae4ec75e33b7ce18aa330f9ba2b6f9
Ekaterina Sychenko <busy.sychenko@gmail.com>
2023-12-05 20:40:36 +0400

    Add game.ts

--- src/logic/game.ts
+++ src/logic/game.ts
@@ -1,100 +1,109 @@
-import Field from '../components/Field';
-
-export type ObjectType<T> = T[keyof T];
-
-export const Theme = {
-  Classic: 'classic',
-  Hearts: 'hearts',
-} as const;
-
-export type ThemeType = ObjectType<typeof Theme>;
-
-export const Player = {
-  Cross: 'cross',
-  Circle: 'circle',
-} as const;
-
-export type PlayerType = ObjectType<typeof Player>;
-
-export const Mark = {
-  Player: Player,
-  Empty: 'empty',
-} as const;
-
-export type MarkType = ObjectType<typeof Mark>;
-
-export const Winner = {
-  Player: Player,
-  Even: 'even',
-} as const;
-
-// const player: PlayerType = Player.Circle;
-
-export type WinnerType = ObjectType<typeof Winner>;
-
-export const GameAction = {
-  ResetGame: 'reset-game',
-  PlaceMark: 'place-mark',
-  ChooseTheme: 'choose-theme',
-} as const;
-
-export type GameActionType = ObjectType<typeof GameAction>;
-
-interface Game {
-  theme: ThemeType;
-  isOver: boolean;
-  isActivePlayer: PlayerType;
-  field: MarkType[];
-  winner?: WinnerType;
-  winningCombination: number[][];
+enum Theme {
+  Classic = 'classic',
+  Hearts = 'hearts',
 }
 
-// TODO: from ArticSpaceCat - "Можно еще типизировать будет default, так чтобы былап ошибка, если не обработал тип в union"
+enum Player {
+  Cross = 'cross',
+  Circle = 'circle',
+}
 
-type Action = { type: GameActionType };
-type ResetGameAction = Action;
-type PlaceMarkAction = { activePlayer: PlayerType; index: number } & Action;
-type ChooseThemeAction = Action;
+function togglePlayer(activePlayer: Player): Player {
+  return activePlayer === Player.Cross ? Player.Circle : Player.Cross;
+}
 
-export type GameActions = ResetGameAction | PlaceMarkAction | ChooseThemeAction;
+type Mark = Player | null;
 
-//
-export default function gameReducer(state: Game, action: GameActions): Game {
+interface ResetGameAction {
+  type: GameAction.ResetGame;
+}
+
+interface PlaceMarkAction {
+  type: GameAction.PlaceMark;
+  index: number;
+  activePlayer: Player;
+}
+
+type Action = ResetGameAction | PlaceMarkAction;
+
+enum GameAction {
+  ResetGame = 'reset game',
+  PlaceMark = 'place mark',
+  ChooseTheme = 'choose theme',
+}
+
+interface Game {
+  theme: Theme;
+  isOver: boolean;
+  activePlayer: Player;
+  field: Mark[];
+  winner: Winner;
+}
+
+enum Winner {
+  Player = 'player',
+  Even = 'even',
+  None = 'none',
+}
+
+export default function gameReducer(state: Game, action: Action): Game {
   switch (action.type) {
     case GameAction.ResetGame: {
       return newGame;
     }
 
     case GameAction.PlaceMark: {
-      if ('activePlayer' in action) {
-        const updatedState: Game = {
-          ...state,
-          isActivePlayer:
-            action.activePlayer === Player.Circle
-              ? Player.Cross
-              : Player.Circle,
-          field: [action.index],
-        };
-      }
+      const updatedField = state.field.map((mark, index) => {
+        if (index === action.index) {
+          const updatedMark = action.activePlayer;
+          return updatedMark;
+        }
+        return mark;
+      });
+
+      const updatedState: Game = {
+        ...state,
+        activePlayer: togglePlayer(action.activePlayer),
+        field: updatedField,
+        winner: calculateWinner(updatedField),
+      };
+      return updatedState;
     }
     default:
-      throw new Error(`No handler found for "${action.type}"`);
+      return state;
+    //   throw new Error(`No handler found for "${action}"`);
   }
 }
 
 export const newGame: Game = {
   theme: Theme.Hearts,
   isOver: false,
-  isActivePlayer: Player.Cross,
-  field: new Array(9).fill(Mark.Empty, 0, 8),
-  winningCombination: [
-    [0, 1, 2],
-    [0, 4, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 4, 6],
-    [2, 5, 8],
-    [3, 4, 5],
-    [6, 7, 8],
-  ],
+  activePlayer: Player.Cross,
+  field: new Array(9).fill(null, 0, 8),
+  winner: Winner.None,
 };
+
+const winningCombination = [
+  [0, 1, 2],
+  [0, 4, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 4, 6],
+  [2, 5, 8],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+function calculateWinner(field: Mark[]): Winner {
+  for (let index = 0; index < winningCombination.length; index++) {
+    const [a, b, c] = winningCombination[index];
+    if (field[a] && field[a] === field[b] && field[a] === field[c]) {
+      return Winner.Player;
+    }
+  }
+
+  if (field.every((item) => item !== null)) {
+    return Winner.Even;
+  }
+  return Winner.None;
+}
